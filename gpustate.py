dd23465d400ace2ebf0a8657098cd3f96a781f15
isrish <net.israel@gmail.com>
2016-05-06 19:21:58 +0200

    removed non-numeric chars

--- gpustate.py
+++ gpustate.py
@@ -13,9 +13,9 @@ def encode_tojson(tree):
 		struct = {}
 		struct['Name']= str(tree.gpu[i].product_name)
 		struct['GPUid'] = 'gpu'+ str(i)
-		struct['MemoryTotal'] = str(tree.gpu[i].fb_memory_usage.total)
-		struct['MemoryUsed'] = str(tree.gpu[i].fb_memory_usage.used)
-		struct['Temperature'] = re.sub('[^0-9]','',str(tree.gpu[i].temperature.gpu_temp))
+		struct['MemoryTotal'] =  re.sub('[^0-9]','', str(tree.gpu[i].fb_memory_usage.total))
+		struct['MemoryUsed'] =  re.sub('[^0-9]','', str(tree.gpu[i].fb_memory_usage.used))
+		struct['Temperature'] = re.sub('[^0-9]','', str(tree.gpu[i].temperature.gpu_temp))
 		struct['FanSpeed'] = re.sub('[^0-9]','',str(tree.gpu[i].fan_speed))
 		struct['PerformanceState'] = str(tree.gpu[i].performance_state)
 		ret.append(struct)
